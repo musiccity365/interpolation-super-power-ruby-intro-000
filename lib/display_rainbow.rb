b59dac6f9e604d776c98f7bb057e428860ebe82f
Michael David Sackstein <m_sackstein@yahoo.com>
2020-09-05 00:19:28 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here
-def display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+def display_rainbow(["R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"])
   puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
   puts "Contact: #{person[3]}"
 end
